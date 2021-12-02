b8696bad18018a05d4b9d8fcf6910aa9ce71ebc5
SomaHiroKS <somahiro1047@gmail.com>
2021-12-03 04:11:40 +0900

    リセットボタンの実装

--- js/main.js
+++ js/main.js
@@ -1,5 +1,12 @@
 var PassSec;   // 秒数カウント用変数
  
+window.onload = function(){
+   document.getElementById("restartcount").disabled = true;
+   document.getElementById("resetcount").disabled = true;
+   document.getElementById("stopcount").disabled = true;
+   console.log("A");
+}
+
 // 繰り返し処理の中身
 function showPassage() {
    PassSec++;
@@ -8,14 +15,33 @@ function showPassage() {
 }
  
 // 繰り返し処理の開始
-function startShowing() {
-   PassSec = 0;   // カウンタのリセット
+function startTimer() {
+   PassSec=0;
    PassageID = setInterval('showPassage()',1000);
    document.getElementById("startcount").disabled = true;
+   document.getElementById("resetcount").disabled = false;
+   document.getElementById("stopcount").disabled = false;
+}
+
+function restartTimer() {
+   PassageID = setInterval('showPassage()',1000);
+   document.getElementById("restartcount").disabled = true;
 }
  
 // 繰り返し処理の中止
-function stopShowing() {
+function stopTimer() {
    clearInterval( PassageID );   // タイマーのクリア
+   document.getElementById("restartcount").disabled = false;
+}
+
+// タイマーリセット
+function resetTimer() {
+   clearInterval( PassageID );   // タイマーのクリア
+   var msg = "ボタンを押してから " + 0 + "秒が経過しました。";
+   document.getElementById("PassageArea").innerHTML = msg;
+   
    document.getElementById("startcount").disabled = false;
+   document.getElementById("stopcount").disabled = true;
+   document.getElementById("restartcount").disabled = true;
+   document.getElementById("resetcount").disabled = true;
 }
